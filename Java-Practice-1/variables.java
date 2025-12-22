728352d394ecdad99b39f798236c05384107dd4e
RajatPanwal <rajatpanwal@gmail.com>
2025-12-23 00:44:44 +0530

    basics

---
+++ Java-Practice-1/variables.java
@@ -0,0 +1,13 @@
+
+public class variables {
+
+    public static void main(String[] args) {
+        int num = 1;
+        byte by = 127;
+        long lg = 5854l;
+        float fl = 5.6f;
+        double db = 5.6;
+        char ch = 'a';
+        boolean bool = true;
+    }
+}
