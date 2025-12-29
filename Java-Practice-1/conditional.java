342761d205f2309d352f7de15e8a223887019b2b
RajatPanwal <rajatpanwal@gmail.com>
2025-12-29 23:57:01 +0530

    info

---
+++ Java-Practice-1/conditional.java
@@ -0,0 +1,15 @@
+
+public class conditional {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        if (a > 3) {
+            System.out.println("A is greater than 3");
+        } else {
+            System.out.println("A is greater less 3");
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Java-fundamentals
 clearing java fundamentals
+
+1. basic java - 
+    - variables
+    - operators
+    - conditional
+    
