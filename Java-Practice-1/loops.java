f8f634e1ee67a15d086ec4f687fadd548bc8c81a
RajatPanwal <rajatpanwal@gmail.com>
2025-12-30 00:35:39 +0530

    loops

---
+++ Java-Practice-1/loops.java
@@ -0,0 +1,25 @@
+
+public class loops {
+
+    public static void main(String[] args){
+
+        // while
+        int i = 1;
+        while(i < 5){
+            System.err.println(i);
+            i++;
+        }
+
+        // for loop
+        for(int j=0; j<5; j++){
+            System.err.println(j);
+        }
+
+        // do while
+        do{
+            System.err.println(i);
+        }while(i<0);
+
+    }
+
+}
