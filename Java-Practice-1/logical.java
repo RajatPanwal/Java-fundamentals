204e472a7259500fe3507351de170f7c4080abd8
RajatPanwal <rajatpanwal@gmail.com>
2025-12-29 23:34:31 +0530

    logical operator

---
+++ Java-Practice-1/logical.java
@@ -0,0 +1,15 @@
+
+public class logical {
+
+    public static void main(String[] args){
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int d = 6;
+
+        boolean result = (a < b) && (c < d);
+
+        System.out.println(result);
+    }
+
+}
