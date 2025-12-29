fdfd123449f0d72f234100d83ab7ce1b95402d33
RajatPanwal <rajatpanwal@gmail.com>
2025-12-30 00:10:16 +0530

    switch case

---
+++ Java-Practice-1/switchcases.java
@@ -0,0 +1,27 @@
+public class switchcases {
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thurs");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Either sat or sun");
+        }
+
+    }
+}
