1bf392b4ae281efb6b2fb81a005927fecc460893
RajatPanwal <rajatpanwal@gmail.com>
2025-12-31 00:23:34 +0530

    method overloading

---
+++ Java-Practice-2/methodOverloading.java
@@ -0,0 +1,27 @@
+
+class Calculator{
+
+    public int add(int a, int b, int c){
+        return a + b + c;
+    }
+    public int add(int a, int b){
+        return a + b ;
+    }
+
+}
+
+public class methodOverloading {
+    
+    public static void main(String[] args){
+
+        Calculator cal = new Calculator();
+
+        int two = cal.add(2,4);
+        System.out.println(two);
+
+        int three = cal.add(2,4, 3);
+        System.out.println(three);
+
+    }
+
+}
