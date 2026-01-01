e3a9e151f583bea14276ec7f6aa32d54cab9c5ca
RajatPanwal <rajatpanwal@gmail.com>
2026-01-01 23:41:03 +0530

    method overloading

--- Java-Practice-2/methodOverloading.java
+++ Java-Practice-2/methodOverloading.java
@@ -1,31 +1,34 @@
 // only change in parameters matters, not even type of method
-class Calculator{
+class Calculator {
 
-    public int add(int a, int b, int c){
+    public int add(int a, int b, int c) {
         return a + b + c;
     }
-    public int add(int a, int b){
-        return a + b ;
+
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public double add(double a, double b){
+    public double add(double a, double b) {
         return a + b;
     }
 
 }
 
 public class methodOverloading {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         Calculator cal = new Calculator();
 
-        int two = cal.add(2,4);
+        int two = cal.add(2, 4);
         System.out.println(two);
 
-        int three = cal.add(2,4, 3);
+        int three = cal.add(2, 4, 3);
         System.out.println(three);
 
+        double dob = cal.add(2.2, 2.2);
+        System.out.println(dob);
 
     }
 
