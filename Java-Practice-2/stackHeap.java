53e1f2d988647c439b6e543d8f42952aa3f7bba3
RajatPanwal <rajatpanwal@gmail.com>
2026-01-03 23:52:16 +0530

    stack and heap memory

---
+++ Java-Practice-2/stackHeap.java
@@ -0,0 +1,22 @@
+
+class Calculator{
+    int num = 5;
+
+    int add(int n1, int n2){
+        return n1 + n2;
+    }
+}
+
+public class stackHeap{
+
+    public static void main (String[] args){
+
+        Calculator obj1 = new Calculator();
+        Calculator obj2 = new Calculator();
+
+        System.out.println(obj1.num);
+        System.out.println(obj2.num);
+
+    }
+
+}
