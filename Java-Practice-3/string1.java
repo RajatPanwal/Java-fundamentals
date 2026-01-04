853e7e0d1857b306e7865ab90b3fbe7ca8993404
RajatPanwal <rajatpanwal@gmail.com>
2026-01-05 00:07:54 +0530

    string in java

---
+++ Java-Practice-3/string1.java
@@ -0,0 +1,14 @@
+
+public class string1{
+
+    public static void main(String[] args){
+
+        String fname = "Rajat";
+        String lname = new String("Panwal");
+        // lname = "Panwal";
+
+        System.out.println(fname + " " + lname);
+
+    }
+
+}
