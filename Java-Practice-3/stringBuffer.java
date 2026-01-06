b9a77b2047f8c13a8a67b27f78b47be79993980b
RajatPanwal <rajatpanwal@gmail.com>
2026-01-07 00:01:05 +0530

    string buffer

---
+++ Java-Practice-3/stringBuffer.java
@@ -0,0 +1,12 @@
+
+public class stringBuffer{
+
+    public static void main(String[] args){
+
+        StringBuffer sb = new StringBuffer();
+
+        System.out.println(sb.capacity());
+
+    }
+
+}
