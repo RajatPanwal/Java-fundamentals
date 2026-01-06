a00fd13a259ac641394b7f98e9504228e197c412
RajatPanwal <rajatpanwal@gmail.com>
2026-01-07 00:21:28 +0530

    static var

--- Java-Practice-3/staticVar.java
+++ Java-Practice-3/staticVar.java
@@ -4,7 +4,7 @@ class Mobile {
     String brand;
     int price;
 
-    // static make the variable common to all the object with same value(address)
+    // static make the variable common to all the object with same value(address) - shared by all objects
     static String name;
 
     void show() {
@@ -20,12 +20,15 @@ public class staticVar {
         Mobile obj1 = new Mobile();
         obj1.brand = "Apple";
         obj1.price = 1500;
-        obj1.name = "SmartPhone";
+        // obj1.name = "SmartPhone";
+
+        // correct way to call a static variable, instead of calling it with an obj 
+        Mobile.name = "SmartPhone";
 
         Mobile obj2 = new Mobile();
         obj2.brand = "Samsung";
         obj2.price = 1300;
-        obj2.name = "SmartPhon";
+        obj2.name = "Phone";
 
         obj1.show();
         obj2.show();
