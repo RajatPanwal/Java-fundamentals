1a6ed6a106f78f66bc2d5a73452b295a70055b8a
RajatPanwal <rajatpanwal@gmail.com>
2026-01-07 00:04:35 +0530

    string buffer

--- Java-Practice-3/stringBuffer.java
+++ Java-Practice-3/stringBuffer.java
@@ -1,11 +1,18 @@
 
-public class stringBuffer{
+public class stringBuffer {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         StringBuffer sb = new StringBuffer();
-
         System.out.println(sb.capacity());
+
+        // string buffer modify same object directly instead of creating new one
+        StringBuffer sb2 = new StringBuffer("Rajat");
+        System.out.println(sb2.capacity());
+
+        sb2.append("Panwal");
+        System.out.println(sb2);
+        System.out.println(sb2.capacity());
 
     }
 
