82d08b94dd04bda1899434cf94d4b6b701e0e13f
RajatPanwal <rajatpanwal@gmail.com>
2026-01-09 22:54:44 +0530

    type of constructors

--- Java-Practice-4/constructor1.java
+++ Java-Practice-4/constructor1.java
@@ -4,9 +4,16 @@ class Human{
     private int age;
     private String name;
 
+    // constructor
     public Human(){
         System.out.println("inside constructor");
     }
+
+    // parameterized constructor
+    public Human(int age, String name){
+        this.age = age;
+        this.name = name;
+    } 
 
     // getter
     public int getAge(){
@@ -30,9 +37,10 @@ public class constructor1{
     public static void main(String[] args){
 
         Human obj = new Human();
-        Human obj2 = new Human();
+        Human obj2 = new Human(20, "Panwal");
 
         System.out.println(obj.getName() + " : " + obj.getAge());
+        System.out.println(obj2.getName() + " : " + obj2.getAge());
 
 
     }
