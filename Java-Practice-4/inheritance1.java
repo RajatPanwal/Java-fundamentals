cb490ffd7edaa8f4360d023c96af60701ed46867
RajatPanwal <rajatpanwal@gmail.com>
2026-01-10 23:35:05 +0530

    multilevel inheritance

--- Java-Practice-4/inheritance1.java
+++ Java-Practice-4/inheritance1.java
@@ -23,21 +23,31 @@ class AdvCalc extends Calc{
 
 }
 
+class VeryAdvCalc extends AdvCalc{
+
+    public int power(int a, int b){
+        return (int)Math.pow(a, b);
+    }
+
+}
+
 public class inheritance1 {
     
     public static void main(String[] args){
 
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
 
         int r1 = obj.add(20, 10);
         int r2 = obj.sub(20, 10);
         int r3 = obj.mul(20, 10);
         int r4 = obj.div(20, 10);
+        int r5 = obj.power(4, 2);
 
         System.out.println(r1);
         System.out.println(r2);
         System.out.println(r3);
         System.out.println(r4);
+        System.out.println(r5);
 
     }
 
