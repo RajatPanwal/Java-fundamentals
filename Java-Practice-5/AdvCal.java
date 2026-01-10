1ea0e0e5593da389a5cb2398cf5e99c7eb6b007d
RajatPanwal <rajatpanwal@gmail.com>
2026-01-11 00:33:34 +0530

    packages

---
+++ Java-Practice-5/AdvCal.java
@@ -0,0 +1,10 @@
+
+package tools;
+
+public class AdvCal extends Cal {
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+}
---
+++ Java-Practice-5/Demo.java
@@ -0,0 +1,11 @@
+import tools.Cal;
+
+public class Demo {
+    
+    public static void main(String[] args){
+
+        Cal obj = new Cal();
+
+    }
+
+}
