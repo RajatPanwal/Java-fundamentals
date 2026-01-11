507878b4afbc32c364ac47f50d0d16d26dfa4b38
RajatPanwal <rajatpanwal@gmail.com>
2026-01-11 23:42:52 +0530

    final method

--- Java-Practice-6/finalClass.java
+++ Java-Practice-6/finalClass.java
@@ -1,23 +1,28 @@
 
-final class Calc{
-    public void add(int a, int b){
-        System.out.println(a+b);
+class Calc {
+    public void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public final void show() {
+        System.out.println("in Calc show");
     }
 }
 
-class AdvCalc extends Calc{
-    public void show(){
-        System.out.println("in show");
+class AdvCalc extends Calc {
+    public void show() {
+        System.out.println("in AdvCalc show");
     }
 }
 
 public class finalClass {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         AdvCalc obj = new AdvCalc();
 
         obj.add(2, 4);
+        obj.show();
 
     }
 
