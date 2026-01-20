0017d142453ed9ba47003afbb6481d63a49d94cd
RajatPanwal <rajatpanwal@gmail.com>
2026-01-20 23:43:37 +0530

    try and catch

--- Java-Practice-9/exception1.java
+++ Java-Practice-9/exception1.java
@@ -3,13 +3,19 @@ public class exception1{
 
     public static void main(String[] args) {
         
-        int i=0, j=0;
+        int i=0, j=2;
+
+        int arr[] = new int[5];
 
         try{
             System.out.println(i/j);
+            System.out.println(arr[5]);
         }
-        catch(Exception e){
+        catch(ArithmeticException e){
             System.out.println("i and j are 0");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array is out of bound");
         }
 
     }
