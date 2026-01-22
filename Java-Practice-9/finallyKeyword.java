0839e7ff266057d797074cc8c90947dee3899501
RajatPanwal <rajatpanwal@gmail.com>
2026-01-22 23:44:48 +0530

    finally

---
+++ Java-Practice-9/finallyKeyword.java
@@ -0,0 +1,19 @@
+
+public class finallyKeyword {
+
+    public static void main(String[] args) {
+
+        int i = 2;
+        int j = 0;
+
+        try {
+            j = 18 / i;
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        } finally {
+            System.out.println("BYE");
+        }
+
+    }
+
+}
