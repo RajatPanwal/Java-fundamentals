e426b88939aadfb081f87c626b273b11f41dc2cb
RajatPanwal <rajatpanwal@gmail.com>
2026-01-23 12:18:07 +0530

    Runnable

---
+++ Java-Practice-10/thread2.java
@@ -0,0 +1,43 @@
+
+class A implements Runnable {
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("HI");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B implements Runnable {
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("HELLO");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class thread2 {
+
+    public static void main(String[] args) {
+        
+        Runnable obj1 = new A();
+        Runnable obj2 = new B();
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start(); 
+
+    }
+    
+}
