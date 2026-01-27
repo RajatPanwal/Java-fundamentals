0dcb831713b889a138b1c00e3c6b780ca30f54a8
RajatPanwal <rajatpanwal@gmail.com>
2026-01-27 23:10:27 +0530

    Array List

--- Java-Practice-11/arrays1.java
+++ Java-Practice-11/arrays1.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class arrays1 {
 
     public static void main(String[] args) {
         
-        Collection<Integer> nums = new ArrayList<Integer>();
+        List<Integer> nums = new ArrayList<Integer>();
 
         nums.add(5);
         nums.add(4);
         nums.add(3);
         nums.add(2);
 
-        System.out.println();
+        System.out.println(nums.get(3));
 
         System.out.println(nums);
 
