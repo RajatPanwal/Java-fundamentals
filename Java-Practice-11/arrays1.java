5889206909f95c39a8de357cb2ec5340cba668d8
RajatPanwal <rajatpanwal@gmail.com>
2026-01-27 23:07:14 +0530

    array list

---
+++ Java-Practice-11/arrays1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class arrays1 {
+
+    public static void main(String[] args) {
+        
+        Collection<Integer> nums = new ArrayList<Integer>();
+
+        nums.add(5);
+        nums.add(4);
+        nums.add(3);
+        nums.add(2);
+
+        System.out.println();
+
+        System.out.println(nums);
+
+    }
+
+}
